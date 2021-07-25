1248d63cae127c91552408ffa8796cf2d52447cb
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-07-25 11:43:36 +0530

    Add files via upload

---
+++ BusinessLayer/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class Employee : IEmployee
+    {
+        public int GetTotalEmployee()
+        {
+            return 10;
+        }
+        
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace Dependancy_Injection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployee _emp = null;
+
+        public HomeController(IEmployee employee)
+        {
+            _emp = employee;
+        }
+        public string Index()
+        {
+            int Count = _emp.GetTotalEmployee();
+            return Count.ToString();
+        }
+
+    }
+}
